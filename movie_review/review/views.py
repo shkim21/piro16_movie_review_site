7cd4d27e13ce029fa2d1b8d9668994438a556e36
sehyeon_kim <shgim9977@gmail.com>
2021-12-23 12:04:52 +0900

    [add] review form 수정

--- movie_review/review/views.py
+++ movie_review/review/views.py
@@ -32,14 +32,31 @@ def review_update(request, pk):
     review = get_object_or_404(Review, id=pk)
 
     if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            review = form.save()
-            return redirect('review:detail', pk)
+        print("request method:", request.method)
+        # form = ReviewForm(request.POST, instance=review)
+        # print("form:", form)
+        print("request.POST:", request.POST)
+        review.title = request.POST.get("title")
+        review.release_date = request.POST.get("release_date")
+        review.genre = request.POST.get("genre")
+        review.rating = request.POST.get("rating")
+        review.running_time = request.POST.get("running_time")
+        review.content = request.POST.get("content")
+        review.director = request.POST.get("director")
+        review.actor = request.POST.get("actor")
+        review.save()
+        return redirect('review:detail', pk)
+
+        # if form.is_valid():
+        #     print("form valid")
+        #     review = form.save()
+        #     return redirect('review:detail', pk)
+        # else:
+        #     print("no valid")
 
     else:
         form = ReviewForm(instance=review)
-        ctx={'form': form}
+        ctx={'form': form, 'review': review}
 
         return render(request, template_name='movie_form.html', context=ctx)
 
