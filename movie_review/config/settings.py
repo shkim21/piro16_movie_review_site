c6e311afc47f41cca5aad7dfa29e29ed4cc21bc4
sehyeon_kim <shgim9977@gmail.com>
2022-01-18 00:35:57 +0900

    media setting

--- movie_review/config/settings.py
+++ movie_review/config/settings.py
@@ -130,3 +130,8 @@ print("staticfiles_dirs:", os.path.join(BASE_DIR, 'static'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
