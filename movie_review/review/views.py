f3c056894cd3d035463a9efc218c1ce9ab7c1d6c
sehyeon_kim <shgim9977@gmail.com>
2022-01-18 03:25:29 +0900

    printing 실습

--- movie_review/review/views.py
+++ movie_review/review/views.py
@@ -6,6 +6,54 @@ from .form import ReviewForm
 
 def review_list(request):
     reviews = Review.objects.all()
+
+    for r in Review.objects.all():
+        s = str(r.id) + ' : ' + r.title + '\n'
+        print(s)
+
+
+    row = Review.objects.get(id=1) # pk는 모델에서 변수로 추가 안해도 생성되는 거 다들 아시죠?
+    print(row.title)
+    print(type(row))#찍히는 객체의 타입도 확인을 해보자.
+
+    rows = Review.objects.filter(title='스파이더맨')
+    print("rows:", rows)
+    print(type(rows))
+
+    rows2 = Review.objects.filter(title='스파이더맨', release_date="2000")
+    print("rows2:", rows2)
+    print("rows2 type:", type(rows2))
+
+    c = Review.objects.count()
+    print("c:", c)
+
+    rows = Review.objects.order_by('-id')
+    for r in rows:
+        print("r id:", r.id, ", r title: ", r.title)
+    
+    dir = Director.objects.get(pk=1)
+    # new_review = Review.objects.create(title="kingsman", director2=dir)
+    # new_review.save()#저장을 해야 db에 반영이 된다!
+
+    lt = Review.objects.filter(release_date__lt = 2020)
+    print("lt:", lt)
+    start = Review.objects.filter(title__startswith="스")
+    print("start:", start)
+
+    all = Review.objects.all()
+    print(all.query)
+    queryset = Review.objects.filter(title__startswith='스') | Review.objects.filter(director__startswith='크')
+    print("queryset:", queryset)
+
+
+    rev1 = Review.objects.all().values('title', 'content')
+    rev2 = Review.objects.all().values_list('title', 'content')
+    rev3 = Review.objects.all().only('title', 'content')
+    print("rev1:", rev1)
+    print("rev2:", rev2)
+    print("rev3:", rev3)
+    print("rev3 type:", type(rev3.first()))
+
     ctx = {'reviews': reviews}
 
     return render(request, template_name='movie_list.html', context=ctx)
@@ -13,6 +61,8 @@ def review_list(request):
 
 def detail(request, pk):
     review = Review.objects.get(id=pk)
+    # review2 = get_object_or_404(Review, pk=10)
+    # print("review2:" ,review2)
     hour = 0
     minute = 0
     if int(review.running_time) > 60:
